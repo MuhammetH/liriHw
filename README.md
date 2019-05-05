d0a38e0037046d5a95846c86b37d0828efb43c48
MuhammetD <doktor.oglu333@gmail.com>
2019-05-05 00:53:19 -0500

    welllllll

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # liriHw
 
 LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+
+liri.js can take in one of the following commands:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+
+
+Ex:  `node liri.js concert-this <artist/band name here>`
+
+Ex:  `node liri.js spotify-this-song '<song name here>'`
+
+Ex:  `node liri.js movie-this '<movie name here>'`
+
+Ex:  `node liri.js do-what-it-says`
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,5 +1,50 @@
+//requires .env file
 require("dotenv").config();
 
-var keys = require("./keys.js");
+//requires request
+let request = require("request");
 
-var spotify = new Spotify(keys.spotify);
+//requires moment
+const moment = require('moment');
+
+//requires filels systems
+const fs = require("fs");
+
+//link to keys file
+const keys = require("./keys.js");
+
+//spotify
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+
+//omdb and bands api
+let omdb = (keys.omdb);
+let bandsintown = (keys.bandsintown)
+
+//user input 
+
+let userInput = process.argv[2];
+let userC = process.argv.slice(3).join(" ");
+
+function userCommand(userInput, userC) {
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-this":
+            doThis(userC);
+            break;
+        default:
+            console.log("I don't understand");
+            break;         
+    }
+}
+
+userCommand(userInput, userC);
+
